9c02c2f456a107d1ed8ffd0d99b32fb5c1e10749
Kuba <korycki.jakub@o2.pl>
2020-02-29 12:05:32 +0100

    jazda

--- DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/DependInfo.cmake
+++ DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/DependInfo.cmake
@@ -4,12 +4,12 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/Kuba/Desktop/cpmasking/Datastructures/DataStructures/BinaryTree.cpp" "C:/Users/Kuba/Desktop/cpmasking/Datastructures/DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/BinaryTree.cpp.obj"
-  "C:/Users/Kuba/Desktop/cpmasking/Datastructures/DataStructures/DList.cpp" "C:/Users/Kuba/Desktop/cpmasking/Datastructures/DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/DList.cpp.obj"
-  "C:/Users/Kuba/Desktop/cpmasking/Datastructures/DataStructures/DynamicArray.cpp" "C:/Users/Kuba/Desktop/cpmasking/Datastructures/DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/DynamicArray.cpp.obj"
-  "C:/Users/Kuba/Desktop/cpmasking/Datastructures/DataStructures/Node.cpp" "C:/Users/Kuba/Desktop/cpmasking/Datastructures/DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/Node.cpp.obj"
-  "C:/Users/Kuba/Desktop/cpmasking/Datastructures/DataStructures/NodeDLL.cpp" "C:/Users/Kuba/Desktop/cpmasking/Datastructures/DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/NodeDLL.cpp.obj"
-  "C:/Users/Kuba/Desktop/cpmasking/Datastructures/DataStructures/main.cpp" "C:/Users/Kuba/Desktop/cpmasking/Datastructures/DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/main.cpp.obj"
+  "C:/Users/Kuba/Desktop/projects/DataStructures/BinaryTree.cpp" "C:/Users/Kuba/Desktop/projects/DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/BinaryTree.cpp.obj"
+  "C:/Users/Kuba/Desktop/projects/DataStructures/DList.cpp" "C:/Users/Kuba/Desktop/projects/DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/DList.cpp.obj"
+  "C:/Users/Kuba/Desktop/projects/DataStructures/DynamicArray.cpp" "C:/Users/Kuba/Desktop/projects/DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/DynamicArray.cpp.obj"
+  "C:/Users/Kuba/Desktop/projects/DataStructures/Node.cpp" "C:/Users/Kuba/Desktop/projects/DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/Node.cpp.obj"
+  "C:/Users/Kuba/Desktop/projects/DataStructures/NodeDLL.cpp" "C:/Users/Kuba/Desktop/projects/DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/NodeDLL.cpp.obj"
+  "C:/Users/Kuba/Desktop/projects/DataStructures/main.cpp" "C:/Users/Kuba/Desktop/projects/DataStructures/cmake-build-debug/CMakeFiles/DataStructures.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "MSVC")
 
--- DataStructures/main.cpp
+++ DataStructures/main.cpp
@@ -15,28 +15,19 @@ using namespace std;;
 DynamicArray dynamicArray;
 ofstream file_out;
 
-void measure_component(string file_name, int* durationContainer, int* sum, int i, int* sizes, int* j, int* w,
-                       int** data_ref) {
-    if(i % 30  == 0 && i != 0) {
-        int average;
-        average = *sum / 30;
-        file_out.open(file_name, fstream::app);
-        file_out << "FOR" << sizes[*j] << "ELEMENTS" << endl;
-        file_out << "AVERAGE :" << average << endl;
-        int stdDeviation = 0;
-        for(int i = 0; i < 30; i++) {
-            stdDeviation = stdDeviation + ((durationContainer[i] - average) * (durationContainer[i] - average));
-        }
-        pow(stdDeviation, 0.5);
-        file_out << "DEVIATION :" << stdDeviation << endl;
-        file_out.close();
-        *w = 0;
-        *sum = 0;
-        if(i != 180) {
-            *j = *j + 1;
-            *data_ref = new int[sizes[*j]];
-        }
+void measure_component(string file_name, int* durationContainer, int sum, int* sizes) {
+    int average;
+    average = sum / 30;
+    file_out.open(file_name, fstream::app);
+    file_out << "FOR" << *sizes << "ELEMENTS" << endl;
+    file_out << "AVERAGE :" << average << endl;
+    int stdDeviation = 0;
+    for(int i = 0; i < 30; i++) {
+        stdDeviation = stdDeviation + ((durationContainer[i] - average) * (durationContainer[i] - average));
     }
+    pow(stdDeviation, 0.5);
+    file_out << "DEVIATION :" << stdDeviation << endl;
+    file_out.close();
 }
 
 void addDA() {
@@ -45,29 +36,32 @@ void addDA() {
     int sum = 0, j = 0;
     int *data_ref = new int[sizes[j]];
 
-    for (int w = 0, i = 0; i <= 180; i++, w++) {
-        for (j; j < 6;) {
+    for (j; j < 6;) {
+        for (int i = 0; i < 30; i++) {
+
             for (int i = 0; i < sizes[j]; i++) {
                 data_ref[i] = rand() % 200 - 100;
-            }
-            for (int i = 0; i < sizes[j]; i++) {
                 dynamicArray.add((double) data_ref[i]);
             }
-            break;
+
+            double item = rand() % 200 - 100;
+            auto t1 = std::chrono::high_resolution_clock::now();
+            dynamicArray.add(item);
+            auto t2 = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+            dynamicArray.remove(item);
+
+            sum += duration;
+            durationContainer[i] = duration;
         }
 
-        double item = rand() % 200 - 100;
-        auto t1 = std::chrono::high_resolution_clock::now();
-        dynamicArray.add(item);
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-        dynamicArray.remove(item);
-        measure_component("addDA.txt", durationContainer,  &sum, i, sizes, &j, &w,
-                          &data_ref);
-
-        sum += duration;
-        durationContainer[w] = duration;
+        measure_component("addDA.txt", durationContainer, sum, &sizes[j]);
+        sum = 0;
+        j++;
+        delete [] data_ref;
+        if(j < 6) data_ref =  new int [sizes[j]];
     }
+
 }
 
 void findDA() {
@@ -76,29 +70,32 @@ void findDA() {
     int sum = 0, j = 0;
     int *data_ref = new int[sizes[j]];
 
-    for (int w = 0, i = 0; i <= 180; i++, w++) {
-        for (j; j < 6;) {
+    for (j; j < 6;) {
+        for (int i = 0; i < 30; i++) {
+
             for (int i = 0; i < sizes[j]; i++) {
                 data_ref[i] = rand() % 200 - 100;
-            }
-            for (int i = 0; i < sizes[j]; i++) {
                 dynamicArray.add((double) data_ref[i]);
             }
-            break;
+
+            double item = rand() % 200 - 100;
+            auto t1 = std::chrono::high_resolution_clock::now();
+            dynamicArray.find(item);
+            auto t2 = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+
+
+            sum += duration;
+            durationContainer[i] = duration;
         }
 
-        double item = rand() % 200 - 100;
-        auto t1 = std::chrono::high_resolution_clock::now();
-        dynamicArray.find(item);
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-
-        measure_component("findDA.txt", durationContainer,  &sum, i, sizes, &j, &w,
-                          &data_ref);
-
-        sum += duration;
-        durationContainer[w] = duration;
+        measure_component("findDA.txt", durationContainer, sum, &sizes[j]);
+        sum = 0;
+        j++;
+        delete [] data_ref;
+        if(j < 6) data_ref =  new int [sizes[j]];
     }
+
 }
 
 void removeDA() {
@@ -107,33 +104,36 @@ void removeDA() {
     int sum = 0, j = 0;
     int *data_ref = new int[sizes[j]];
 
-    for (int w = 0, i = 0; i <= 180; i++, w++) {
-        for (j; j < 6;) {
+    for (j; j < 6;) {
+        for (int i = 0; i < 30; i++) {
+
             for (int i = 0; i < sizes[j]; i++) {
                 data_ref[i] = rand() % 200 - 100;
-            }
-            for (int i = 0; i < sizes[j]; i++) {
                 dynamicArray.add((double) data_ref[i]);
             }
-            break;
+
+            double item = rand() % 200 - 100;
+            auto t1 = std::chrono::high_resolution_clock::now();
+            dynamicArray.remove(item);
+            auto t2 = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+
+            sum += duration;
+            durationContainer[i] = duration;
         }
 
-        double item = rand() % 200 - 100;
-        auto t1 = std::chrono::high_resolution_clock::now();
-        dynamicArray.remove(item);
-        auto t2 = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-
-        measure_component("RemoveDA.txt", durationContainer,  &sum, i, sizes, &j, &w,
-                &data_ref);
-
-        sum += duration;
-        durationContainer[w] = duration;
+        measure_component("removeDA.txt", durationContainer, sum, &sizes[j]);
+        sum = 0;
+        j++;
+        delete [] data_ref;
+        if(j < 6) data_ref =  new int [sizes[j]];
     }
+
 }
+
 int main() {
     srand(time(NULL));
-    //removeDA();
- // addDA();
-   //findDA();
+     removeDA();
+    //addDA();
+    //findDA();
 }
